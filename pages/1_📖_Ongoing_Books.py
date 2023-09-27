cdfd6f446184127f473882cf06314e2602690cfc
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-09-28 00:38:34 +0600

    Update 1_📖_Ongoing_Books.py

--- pages/1_📖_Ongoing_Books.py
+++ pages/1_📖_Ongoing_Books.py
@@ -100,8 +100,7 @@ if __name__ == '__main__':
         for i in range(len(x)):
             if 'completed' in data.columns:
                 plt.text(i, round(y[i]//2), str(round(y[i])+"%", ha = 'center',fontsize='medium', color='white')
-            else:
-                plt.text(i, round(y[i]//2), round(y[i]), ha = 'center',fontsize='medium', color='white')
+            plt.text(i, round(y[i]//2), round(y[i]), ha = 'center',fontsize='medium', color='white')
         
     df=pd.read_csv('./data/books.csv')
     selected_option=st.radio("Select",["Show Chart","Show Data"],horizontal=True)
