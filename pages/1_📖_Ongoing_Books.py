d6fb74ee0511b30bb828286396f5b8de80cb4760
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-09-28 00:29:46 +0600

    Update 1_📖_Ongoing_Books.py

--- pages/1_📖_Ongoing_Books.py
+++ pages/1_📖_Ongoing_Books.py
@@ -99,7 +99,7 @@ if __name__ == '__main__':
     def addlabels(x,y,data):
         x,y = list(data[x]),list(data[y])
         for i in range(len(x)):
-            plt.text(i, y[i]//2, y[i], ha = 'center',fontsize='medium', color='white')
+            plt.text(i, round(y[i]//2), y[i], ha = 'center',fontsize='medium', color='white')
         
     df=pd.read_csv('./data/books.csv')
     selected_option=st.radio("Select",["Show Chart","Show Data"],horizontal=True)
