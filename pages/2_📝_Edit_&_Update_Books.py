2b90c14cb973086b2052d07237123c7c9f619202
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-10-01 00:04:22 +0600

    Update 2_📝_Edit_&_Update_Books.py

--- pages/2_📝_Edit_&_Update_Books.py
+++ pages/2_📝_Edit_&_Update_Books.py
@@ -67,12 +67,12 @@ def main(df,df_exp,books_df):
             st.dataframe(upd)
             if st.button("Confirm"):
                 save_data(df_exp)
-                exp.empty()
                 subs=[]
+                exp.empty()
             elif st.button("Cancel"):
                 save_data(df)
-                exp.empty()
                 subs=[]
+                exp.empty()
         else:
             st.write("`No Changes Detected`")
 
