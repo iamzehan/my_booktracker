8c84fbb466db17f6c31e489b117ee67f6fb5c2c6
Zehan <43857150+iamzehan@users.noreply.github.com>
2023-12-17 01:00:58 +0600

    Update 3_📚_Add_New_Books.py

--- pages/3_📚_Add_New_Books.py
+++ pages/3_📚_Add_New_Books.py
@@ -1,6 +1,7 @@
 import datetime
 import pandas as pd
 import streamlit as st
+from Book_Tracker import check_password
 
 def save_data(data):
     return data.to_csv('./data/books.csv',index=False)
@@ -40,45 +41,45 @@ def main(df,df_exp):
             ### If after submission you want to reset, go to the len(df) and delete that row###
 
 if __name__ == "__main__":
+    if check_passoword():
+        #config
+        st.set_page_config(page_title="Add Book", page_icon="📚")
     
-    #config
-    st.set_page_config(page_title="Add Book", page_icon="📚")
-
-    hide_streamlit_style = """
-            <head>
-            <style>
-            #MainMenu{visibility: hidden;}
-            .css-fk4es0{display:none;}
-            .css-1lsmgbg {display: none;}
-            .myFooter{color:rgba(250, 250, 250, 0.6); margin-top: 150px; text-align: center;}
-            .myFooter a{color: rgb(255, 75, 75); font-weight: bolder;}
-            .css-10trblm{color:rgb(255, 75, 75); text-align:center;}
-            .css-16huue1 {color:rgb(255, 75, 75); font-size:18px;}
-            .css-v37k9u p{color:#edf5e1; font-size: 18px;}
-            .css-1q8dd3e{color:rgb(255, 75, 75);}
-            .css-1q8dd3e:hover{color:#edf5e1; border-color:rgb(255, 75, 75);}
-            .css-17ziqus {background-color: brown; visibility: visible}
-            .css-ffhzg2 {text-align: center;}
-            </style>
-            <title> Book Tracker </title>
-            </head>
-            <script>document.getElementById("demo").innerHTML = new Date().getFullYear();</script>
-            """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    st.header("📚 Add a new Book")
-    st.sidebar.header("📚 Add a new Book")
-    st.markdown("Add a new book that you are planning to read")
-
-    if 'df' not in st.session_state:
-            st.session_state.df = pd.read_csv('./data/books.csv')
-    df = st.session_state.df
+        hide_streamlit_style = """
+                <head>
+                <style>
+                #MainMenu{visibility: hidden;}
+                .css-fk4es0{display:none;}
+                .css-1lsmgbg {display: none;}
+                .myFooter{color:rgba(250, 250, 250, 0.6); margin-top: 150px; text-align: center;}
+                .myFooter a{color: rgb(255, 75, 75); font-weight: bolder;}
+                .css-10trblm{color:rgb(255, 75, 75); text-align:center;}
+                .css-16huue1 {color:rgb(255, 75, 75); font-size:18px;}
+                .css-v37k9u p{color:#edf5e1; font-size: 18px;}
+                .css-1q8dd3e{color:rgb(255, 75, 75);}
+                .css-1q8dd3e:hover{color:#edf5e1; border-color:rgb(255, 75, 75);}
+                .css-17ziqus {background-color: brown; visibility: visible}
+                .css-ffhzg2 {text-align: center;}
+                </style>
+                <title> Book Tracker </title>
+                </head>
+                <script>document.getElementById("demo").innerHTML = new Date().getFullYear();</script>
+                """
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+        st.header("📚 Add a new Book")
+        st.sidebar.header("📚 Add a new Book")
+        st.markdown("Add a new book that you are planning to read")
     
-    #df=pd.read_csv('./data/books.csv')
-    df_exp=df.copy()
-
-    main(df,df_exp)
-
-    current_year = datetime.datetime.now().year
-    footer = f'© {current_year} Copyright | Made by <a href="https://ziaulkarim.netlify.app" >Md. Ziaul Karim</a>'
-    hide_streamlit_style = f"""<div class="myFooter">{footer}</a> </div>"""
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+        if 'df' not in st.session_state:
+                st.session_state.df = pd.read_csv('./data/books.csv')
+        df = st.session_state.df
+        
+        #df=pd.read_csv('./data/books.csv')
+        df_exp=df.copy()
+    
+        main(df,df_exp)
+    
+        current_year = datetime.datetime.now().year
+        footer = f'© {current_year} Copyright | Made by <a href="https://ziaulkarim.netlify.app" >Md. Ziaul Karim</a>'
+        hide_streamlit_style = f"""<div class="myFooter">{footer}</a> </div>"""
+        st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
